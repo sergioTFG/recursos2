3b952564eb74eb6f5abd62468083cec8abb67146
sergioTFG <pksergioest@gmail.com>
2024-06-18 12:28:10 +0200

    Update main.tf

--- templates/main.tf
+++ templates/main.tf
@@ -1,12 +1,15 @@
-
+# Crear backend de Terraform.
 terraform {
   backend "azurerm" {
-    resource_group_name   = "dev13"
-    storage_account_name  = "storageiesgn1"
-    container_name        = "contenedoriesgn1"
-    key                   = "terraform.tfstate"
+    resource_group_name  = "App_Tareas"
+    storage_account_name = "appalmacen"
+    container_name       = "contenedoralmacen"
+    key                  = "terraform.tfstate"
   }
+}
 
+# Seleccionar el Azure Provider y la versión que usará
+terraform {
   required_version = ">= 0.14"
 
   required_providers {
@@ -17,15 +20,18 @@ terraform {
   }
 }
 
+# Configurar Microsoft Azure provider
 provider "azurerm" {
   features {}
 }
 
+# Crear grupo de recursos en caso de que no exista
 resource "azurerm_resource_group" "az_rg" {
   name     = var.ResourceGroupName
   location = var.ResourceGroupLocation
 }
 
+# Crear Virtual Network
 resource "azurerm_virtual_network" "az_vnet" {
   name                = var.AzurermVirtualNetworkName
   location            = azurerm_resource_group.az_rg.location
@@ -37,6 +43,7 @@ resource "azurerm_virtual_network" "az_vnet" {
   }
 }
 
+# Crear Subnet en la Virtual Network
 resource "azurerm_subnet" "az_subnet" {
   name                 = "my-terraform-subnet"
   resource_group_name  = azurerm_resource_group.az_rg.name
@@ -44,6 +51,7 @@ resource "azurerm_subnet" "az_subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# Crear IP Pública
 resource "azurerm_public_ip" "az_public_ip" {
   name                = "my-terraform-public-ip"
   location            = azurerm_resource_group.az_rg.location
@@ -55,6 +63,7 @@ resource "azurerm_public_ip" "az_public_ip" {
   }
 }
 
+# Crear Network Security Group y reglas
 resource "azurerm_network_security_group" "az_net_sec_group" {
   name                = "my-terraform-nsg"
   location            = azurerm_resource_group.az_rg.location
@@ -101,6 +110,7 @@ resource "azurerm_network_security_group" "az_net_sec_group" {
   }
 }
 
+# Crear Network Interface
 resource "azurerm_network_interface" "az_net_int" {
   name                = "my-terraform-nic"
   location            = azurerm_resource_group.az_rg.location
@@ -119,11 +129,13 @@ resource "azurerm_network_interface" "az_net_int" {
   }
 }
 
+# Crear Network Interface Security Group association
 resource "azurerm_network_interface_security_group_association" "az_net_int_sec_group" {
   network_interface_id      = azurerm_network_interface.az_net_int.id
   network_security_group_id = azurerm_network_security_group.az_net_sec_group.id
 }
 
+# Crear máquina virtual
 resource "azurerm_linux_virtual_machine" "az_linux_vm" {
   name                            = "my-terraform-vm"
   location                        = azurerm_resource_group.az_rg.location
@@ -153,8 +165,9 @@ resource "azurerm_linux_virtual_machine" "az_linux_vm" {
   }
 }
 
+# Configurar la ejecución de tareas automáticamente al iniciar la MV.
 resource "azurerm_virtual_machine_extension" "az_vm_extension" {
-  name                 = "hostname"
+  name                 = "customScript"
   virtual_machine_id   = azurerm_linux_virtual_machine.az_linux_vm.id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
@@ -162,7 +175,7 @@ resource "azurerm_virtual_machine_extension" "az_vm_extension" {
 
   settings = <<SETTINGS
     {
-      "commandToExecute": "apt-get update && apt-get install -y apache2 git && git clone ${var.app_repo_url} /var/www/html/ && systemctl restart apache2"
+      "commandToExecute": "apt-get update && apt-get install -y apache2 git && mkdir -p /var/www/myapp && git clone ${var.github_repo} /var/www/myapp"
     }
   SETTINGS
 
@@ -171,11 +184,86 @@ resource "azurerm_virtual_machine_extension" "az_vm_extension" {
   }
 }
 
+
+# Fuente de datos para acceder a las propiedades de una dirección IP pública de Azure existente.
 data "azurerm_public_ip" "az_public_ip" {
   name                = azurerm_public_ip.az_public_ip.name
   resource_group_name = azurerm_linux_virtual_machine.az_linux_vm.resource_group_name
 }
 
+# variables:
+
+# Variable de salida: Dirección de IP pública
 output "public_ip" {
   value = data.azurerm_public_ip.az_public_ip.ip_address
 }
+
+variable "StorageAccountName" {
+  type        = string
+  default     = "storage01" 
+  description = "Nombre para la cuenta de almacenamiento"
+}
+
+variable "ResourceGroupName" {
+  type        = string 
+  default     = "my-terraform-rg" 
+  description = "Nombre del grupo de recursos"
+}
+
+variable "ResourceGroupLocation" {
+  type        = string 
+  default     = "West Europe" 
+  description = "Región donde reside el grupo de recursos"
+}
+
+variable "AzurermVirtualNetworkName" {
+  type        = string
+  default     = "my-terraform-vnet"
+  description = "Nombre para la red virtual de Azure"
+}
+
+# Configuración VM
+variable "usuario" {
+  type        = string 
+  default     = "Sergio"
+  sensitive   = true 
+}
+
+variable "contrasena" {
+  type        = string 
+  default     = "Usuario1!"
+  sensitive   = true
+}
+
+variable "hostname" {
+  type        = string 
+  default     = "myvm" 
+}
+
+variable "ip_privada" {
+  type        = string
+  default     = "10.0.2.5"
+}
+
+variable "size" {
+  type        = string
+  default     = "Standard_DS1_v2"
+}
+
+# Variables httpd
+variable "puerto_expuesto" {
+  type        = string
+  default     = "8080"
+}
+
+variable "salida_echo" {
+  type        = string
+  default     = "Hola mundo"
+}
+
+variable "github_repo" {
+  type        = string
+  description = "URL del repositorio de GitHub que contiene la aplicación"
+  default     = "https://github.com/sergioTFG/Gestor"
+}
+
